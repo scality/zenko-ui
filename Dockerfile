3eeadcfba64bfaf27040675409d61c09b81aaf13
Nicolas Humbert <nicolas.humbert@live.fr>
2020-04-07 09:24:07 -0700

    ZENKO-2504 Package Zenko UI

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+ARG NGINX_IMAGE_VERSION=1.15.8
+
+FROM nginx:${NGINX_IMAGE_VERSION}
+
+EXPOSE 8383
+
+COPY conf/zenko-ui-nginx.conf /etc/nginx/conf.d/default.conf
+
+RUN rm -rf /usr/share/nginx/html/*
+
+COPY public/assets/ /usr/share/nginx/html/
+
+CMD ["nginx", "-g", "daemon off;"]
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,7 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
+    mode: 'production',
     devtool: 'hidden-source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production'),
-        }),
-    ],
 });
