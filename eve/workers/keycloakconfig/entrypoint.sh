c0910519163a99b777e973c5cbebf272cccd2ee1
Nicolas Humbert <nicolas.humbert@live.fr>
2020-06-23 12:37:20 -0700

    ZENKO-2659 E2E test user authentication

---
+++ eve/workers/keycloakconfig/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# set -e stops the execution of a script if a command or pipeline has an error
+set -e
+
+# modifying keycloak-realm.json
+JQ_FILTERS_REALM="."
+
+if [[ "$KEYCLOAK_REALM" ]] ; then
+    JQ_FILTERS_REALM="$JQ_FILTERS_REALM | .realm=\"$KEYCLOAK_REALM\""
+fi
+
+if [[ "$KEYCLOAK_CLIENT_ID" ]] ; then
+    JQ_FILTERS_REALM="$JQ_FILTERS_REALM | .clients[0].clientId=\"$KEYCLOAK_CLIENT_ID\""
+fi
+
+if [[ "$KEYCLOAK_USERNAME" ]] ; then
+    JQ_FILTERS_REALM="$JQ_FILTERS_REALM | .users[0].username=\"$KEYCLOAK_USERNAME\""
+fi
+
+if [[ "$KEYCLOAK_USER_FIRSTNAME" ]] ; then
+    JQ_FILTERS_REALM="$JQ_FILTERS_REALM | .users[0].firstName=\"$KEYCLOAK_USER_FIRSTNAME\""
+fi
+
+if [[ "$KEYCLOAK_USER_LASTNAME" ]] ; then
+    JQ_FILTERS_REALM="$JQ_FILTERS_REALM | .users[0].lastName=\"$KEYCLOAK_USER_LASTNAME\""
+fi
+
+if [[ $JQ_FILTERS_REALM != "." ]]; then
+    jq "$JQ_FILTERS_REALM" keycloak-realm.json > keycloak-realm.json.tmp
+    mv keycloak-realm.json.tmp keycloak-realm.json
+fi
+
+# LAUNCH keycloak entrypoint
+/opt/jboss/tools/docker-entrypoint.sh $@
