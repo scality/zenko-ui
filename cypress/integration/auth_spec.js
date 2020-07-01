b7999d878ccd81357a06f17ee153d8f33fa66b74
Nicolas Humbert <nicolas.humbert@live.fr>
2020-07-01 15:31:49 -0700

    cleanup 2

--- cypress/integration/auth_spec.js
+++ cypress/integration/auth_spec.js
@@ -1,7 +1,4 @@
 /* eslint jest/expect-expect: 0 */
-const userProfile = {
-    name: 'Nicolas Humbert',
-};
 
 describe('Authentication with keycloak', () => {
     describe('User authenticated', () => {
@@ -12,37 +9,12 @@ describe('Authentication with keycloak', () => {
         it('should render user name', () =>  {
             cy.visit('/');
             cy.get('.sc-navbar').should('exist');
-            cy.get('.sc-navbar').should('contain', userProfile.name);
+            // NOTE: this value is based on "eve/workers/keycloakconfig/keycloak-realm.json"
+            cy.get('.sc-navbar').should('contain', 'Nicolas Humbert');
         });
 
         afterEach(() =>  {
             cy.kcLogout();
-        });
-    });
-
-    describe('User not authenticated', () => {
-        it('should not render user name', () =>  {
-            cy.visit('/');
-            cy.get('.sc-navbar').should('not.exist');
-            cy.url();
-        });
-    });
-});
-
-describe('Authentication with mocked authentication process', () => {
-    describe('User authenticated', () => {
-        beforeEach(() => {
-            cy.kcFakeLogin(userProfile);
-        });
-
-        it('should render user name', () =>  {
-            cy.visit('/');
-            cy.get('.sc-navbar').should('exist');
-            cy.get('.sc-navbar').should('contain', userProfile.name);
-        });
-
-        afterEach(() =>  {
-            cy.clearSession();
         });
     });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,3 @@
-function getAuthCodeFromLocation(location: string): string | undefined {
-    const url = new URL(location);
-    const params = url.search.substring(1).split('&');
-    for (const param of params) {
-        const [key, value] = param.split('=');
-        if (key === 'code') {
-            return value;
-        }
-    }
-}
-
 Cypress.Commands.add('kcLogin', (username, password) => {
     Cypress.log({ name: 'Login' });
     const kcUsername =  username || Cypress.env('KEYCLOAK_USERNAME');
@@ -61,24 +50,6 @@ Cypress.Commands.add('kcLogout', () => {
     return cy.request({
         url: `${keycloakRoot}/auth/realms/${keycloakRealm}/protocol/openid-connect/logout`,
     });
-});
-
-Cypress.Commands.add('kcFakeLogin', (userProfile) => {
-    Cypress.log({ name: 'Fake Login' });
-    const keycloakRoot = Cypress.env('KEYCLOAK_ROOT') || 'http://127.0.0.1:8080';
-    const keycloakRealm = Cypress.env('KEYCLOAK_REALM') || 'myrealm';
-    const keycloakClientID = Cypress.env('KEYCLOAK_CLIENT_ID') || 'myclient';
-    const t = {
-        access_token: 'fake_access_token',
-        expires_at: 15 * 60 * 1000 + new Date().getTime(),
-        id_token: 'fake_id_token',
-        profile: userProfile || {},
-        refresh_token: 'fake_refresh_token',
-        scope: 'openid profile email',
-        session_state: 'fake_session_state',
-        token_type: 'bearer',
-    };
-    window.sessionStorage.setItem(`oidc.user:${keycloakRoot}/auth/realms/${keycloakRealm}:${keycloakClientID}`, JSON.stringify(t));
 });
 
 Cypress.Commands.add('clearSession', () => {
