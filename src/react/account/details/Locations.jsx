d26cea8b034c480ed7da443f9f1e5e10c473cf37
Nicolas Humbert <nicolas.humbert@live.fr>
2020-08-24 17:18:28 -0700

    wording

--- src/react/account/details/Locations.jsx
+++ src/react/account/details/Locations.jsx
@@ -55,7 +55,7 @@ const Overlay = styled.div`
 
 const CustomHeader = () => <span>
     Target Bucket
-    <Tooltip overlay={<Overlay> Name of the bucket/container created in the specific location (e.g. RING, Azure, AWS S3, GCP...) and where the data of your Zenko buckets that are attached to that location will be stored. </Overlay>} placement="right">
+    <Tooltip overlay={<Overlay> Name of the bucket/container created in the specific location (e.g. RING, Azure, AWS S3, GCP...), and where the Zenko buckets attached to that location will store data. </Overlay>} placement="right">
         <IconTooltip className='far fa-question-circle'></IconTooltip >
     </Tooltip>
 </span>;
