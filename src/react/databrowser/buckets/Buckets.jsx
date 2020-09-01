4840de2fa043d7e7459963fd81fde9a02b32e7bb
Nicolas Humbert <nicolas.humbert@live.fr>
2020-08-31 17:20:41 -0700

    yes

--- src/react/databrowser/buckets/Buckets.jsx
+++ src/react/databrowser/buckets/Buckets.jsx
@@ -1,24 +1,42 @@
 // @flow
 import * as L from '../../ui-elements/ListLayout2';
-
+import { useDispatch, useSelector } from 'react-redux';
 import type { AppState } from '../../../types/state';
 import { Breadcrumb } from '@scality/core-ui';
 import BucketHead from './BucketHead';
 import BucketList from './BucketList';
 import React from 'react';
+import { assumeRoleWithWebIdentity } from '../../actions';
+import styled from 'styled-components';
 
-import { useSelector } from 'react-redux';
+const Select = styled.select`
+    outline: 0px;
+    font-size: inherit;
+`;
 
 export default function Buckets(){
+    const dispatch = useDispatch();
+
     const buckets = useSelector((state: AppState) => state.s3.listBucketsResults.list);
     const accountName = useSelector((state: AppState) => state.s3.listBucketsResults.ownerName);
     const locations = useSelector((state: AppState) => state.configuration.latest.locations);
-    console.log('accountName!!!', accountName);
+    const accounts = useSelector((state: AppState) => state.configuration.latest.users);
+
+    const switchAccount = accountName => {
+        const account = accountName && accounts.find(a => a.userName === accountName);
+        if (!account) {
+            return;
+        }
+        dispatch(assumeRoleWithWebIdentity(`arn:aws:iam::${account.id}:role/roleForB`));
+    };
+
     return <L.Container>
         <L.BreadcrumbContainer>
             <Breadcrumb
                 paths={[
-                    <label key={1}>{ accountName }</label>,
+                    <Select key={1} onChange={e => switchAccount(e.target.value) } name="cars" id="cars">
+                        { accounts.map(account => <option key={account.userName} selected={account.userName === accountName} value={account.userName}>{account.userName}</option>)}
+                    </Select>,
                     <label key={2}>buckets</label>,
                 ]}
             />
