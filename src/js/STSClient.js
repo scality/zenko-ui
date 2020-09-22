ae9d4bda5c0ea3ef305571ecca32c2249ab19624
Nicolas Humbert <nicolas.humbert@live.fr>
2020-09-22 12:26:44 +0200

    mock assumeRoleWithWebIdentity

--- src/js/STSClient.js
+++ src/js/STSClient.js
@@ -8,35 +8,26 @@ export default class STSClient {
         });
     }
 
-    assumeRoleWithWebIdentity(params) {
-        const { idToken, roleArn } = params;
-        const p = {
-            DurationSeconds: 900, // 15 minutes
-            RoleArn: roleArn,
-            RoleSessionName: 'zenko-ui',
-            WebIdentityToken: idToken,
-        };
-        return this.client.assumeRoleWithWebIdentity(p).promise();
+    // assumeRoleWithWebIdentity(params) {
+    //     const { idToken, roleArn } = params;
+    //     const p = {
+    //         DurationSeconds: 900, // 15 minutes
+    //         RoleArn: roleArn,
+    //         RoleSessionName: 'zenko-ui',
+    //         WebIdentityToken: idToken,
+    //     };
+    //     return this.client.assumeRoleWithWebIdentity(p).promise();
+    // }
+
+    // TODO: Testing only, remove it once STS.assumeRoleWithWebIdentity is implemented in Vault.
+    assumeRoleWithWebIdentity() {
+        return Promise.resolve({
+            Credentials: {
+                AccessKeyId: 'accessKey1',
+                SecretAccessKey: 'verySecretKey1',
+                // SessionToken: 'AQoDYXdzEE0a8ANXXXXXXXXNO1ewxE5TijQyp+IEXAMPLE',
+            },
+        });
     }
 
 }
-
-// TODO: Testing only, remove it once STS.assumeRoleWithWebIdentity is implemented in Vault.
-// export default class STSClient {
-//     constructor(conf) {
-//         this.client = new AWS.STS({
-//             endpoint: conf.endpoint,
-//         });
-//     }
-//
-//     assumeRoleWithWebIdentity() {
-//         return Promise.resolve({
-//             Credentials: {
-//                 AccessKeyId: 'accessKey1',
-//                 SecretAccessKey: 'verySecretKey1',
-//                 // SessionToken: 'AQoDYXdzEE0a8ANXXXXXXXXNO1ewxE5TijQyp+IEXAMPLE',
-//             },
-//         });
-//     }
-//
-// }
