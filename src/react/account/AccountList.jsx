83cf7b118c1054ef5266c09bd30f500193e9ff5d
theocerutti <theo.cerutti@epitech.eu>
2021-04-13 09:35:00 +0200

    ZENKO-3184 'Created On' Column format to adjust on Account list

--- src/react/account/AccountList.jsx
+++ src/react/account/AccountList.jsx
@@ -7,7 +7,7 @@ import type { Account } from '../../types/account';
 import { AutoSizer } from 'react-virtualized';
 import { FixedSizeList } from 'react-window';
 import { ListSection } from '../ui-elements/ListLayout';
-import { formatDate } from '../utils';
+import { formatSimpleDate } from '../utils';
 import { push } from 'connected-react-router';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
@@ -27,7 +27,8 @@ const columns = [
     {
         Header: 'Created on',
         accessor: 'createDate',
-        Cell: ({ value }) => { return formatDate(new Date(value));},
+        headerStyle: { textAlign: 'right' },
+        Cell: ({ value }) => <div style={{ textAlign: 'right' }}>{formatSimpleDate(new Date(value))}</div>,
     },
 ];
 
@@ -75,7 +76,7 @@ function AccountList({ accountList, accountIndex }: Props) {
                         {headerGroups.map(headerGroup => (
                             <T.HeadRow key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
                                 {headerGroup.headers.map(column => (
-                                    <T.HeadCell onClick={() => handleSortClick(column, listRef)} key={column.id} {...column.getHeaderProps()} >
+                                    <T.HeadCell onClick={() => handleSortClick(column, listRef)} key={column.id} {...column.getHeaderProps()} style={{ ...column.headerStyle }}>
                                         {column.render('Header')}
                                         <T.Icon>
                                             {column.isSorted
