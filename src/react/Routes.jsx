b6ea83b21a4decd99604fa74f00e04092d739134
Nicolas Humbert <nicolas.humbert@scality.com>
2021-04-15 22:07:32 +0200

    ZENKO-3380 Update API stats and status every 10 seconds

--- src/react/Routes.jsx
+++ src/react/Routes.jsx
@@ -2,6 +2,7 @@
 import { NavbarContainer, RouteContainer } from './ui-elements/Container';
 import React, { useEffect } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { loadClients, loadInstanceLatestStatus, loadInstanceStats } from './actions';
 import { useDispatch, useSelector } from 'react-redux';
 import AccountCreate from './account/AccountCreate';
 import Accounts from './account/Accounts';
@@ -12,7 +13,6 @@ import LocationEditor from './backend/location/LocationEditor';
 import { Navbar } from './Navbar';
 import NoMatch from './NoMatch';
 import Workflows from './workflow/Workflows';
-import { loadClients } from './actions';
 
 function PrivateRoutes() {
     const dispatch = useDispatch();
@@ -26,6 +26,15 @@ function PrivateRoutes() {
             // TODO: forbid loading clients when authorization server redirects the user back to ui.zenko.local with an authorization code.
             // That will fix management API request being canceled during autentication.
             dispatch(loadClients());
+
+            const refreshIntervalStatsUnit = setInterval(
+                () => dispatch(loadInstanceLatestStatus()), 10000);
+            const refreshIntervalStatsSeries = setInterval(
+                () => dispatch(loadInstanceStats()), 10000);
+            return () => {
+                clearInterval(refreshIntervalStatsUnit);
+                clearInterval(refreshIntervalStatsSeries);
+            };
         }
     },[dispatch, user]);
 
