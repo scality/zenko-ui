24b5d4cb7d75039a71bb1a14a911b64e62a08b2a
Nicolas Humbert <nicolas.humbert@scality.com>
2021-06-08 11:33:24 -0500

    ZENKO-3557 Upgrade webpack to v5

--- webpack.common.js
+++ webpack.common.js
@@ -9,6 +9,7 @@ module.exports = {
         filename: 'js/[name].js',
         path: path.resolve(__dirname, 'public/assets'),
         publicPath: '/',
+        assetModuleFilename: 'fonts/[name][ext]?[contenthash]',
     },
     resolve: {
         modules: ['node_modules'],
@@ -34,32 +35,15 @@ module.exports = {
             },
             {
                 test: /\.jpe?g$|\.gif$|\.png$|\.ttf$|\.eot$|\.svg$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]?[hash]',
-                            outputPath: 'fonts/',
-                        },
-                    },
-                ],
+                type: 'asset/resource',
             },
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 10000,
-                            mimetype: 'application/fontwoff',
-                            outputPath: 'fonts/',
-                        },
-                    },
-                ],
+                type: 'asset',
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader',
+                use: ['style-loader', 'css-loader'],
             },
         ],
     },
