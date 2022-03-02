3bf20a45ed635dd45dec522440cc2123f753187c
Asmaa EL MOKHTARI <100131224+AsmaaELM@users.noreply.github.com>
2022-03-02 18:29:40 +0100

    Delete users with refactor queries

--- src/react/Queries.jsx
+++ src/react/Queries.jsx
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { useAwsPaginatedEntities } from './utils/IAMhooks';
-import { useIAMClient } from './IAMProvider';
 
 export const getUserAccessKeysQuery = (userName, IAMClient) => ({
   queryKey: ['listIAMUserAccessKey', userName],
