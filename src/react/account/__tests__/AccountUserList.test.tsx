a868285dc6f4352a0104d697803baa26a121f326
Asmaa EL MOKHTARI <100131224+AsmaaELM@users.noreply.github.com>
2022-03-16 14:56:06 +0100

    Revert "Testing account users"
    This reverts commit 2ee7e166fb8ec91f97fec8b337c89df0522f2a0e.

--- src/react/account/__tests__/AccountUserList.test.tsx
+++ src/react/account/__tests__/AccountUserList.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent, getByText, getAllByRole } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
@@ -14,9 +14,9 @@ const SAMPLE_USER_ID = 'GENERATED_ID';
 const SAMPLE_USER_NAME = 'test';
 const SAMPLE_CREATE_DATE = '2022-03-02T08:35:24Z';
 const SAMPLE_ARN = `arn:aws:iam::970343539682:user/${SAMPLE_USER_NAME}`;
+const nbrOfColumnsExpected = 4;
 
 // AutoSizer uses offsetWidth and offsetHeight.
-// Jest runs in JSDom which doesn't support measurements APIs.
 function mockOffsetSize(width: number, height: number) {
   Object.defineProperty(window, 'getComputedStyle', {
     value: () => {
@@ -123,6 +123,61 @@ describe('AccountUserList', () => {
     //Ensure user is displayed in the table once the loading complete
     expect(screen.getByText(SAMPLE_USER_NAME)).toBeInTheDocument();
 
-    //TODO test copy arn
+    const firstRow = screen.getAllByRole('row')[1];
+
+    const arnButton = screen.getByText('Copy ARN')
+    expect(arnButton).toBeInTheDocument();
+
+    const eyeButton = screen.getByLabelText('Checking or creating access keys')
+    expect(eyeButton).toBeInTheDocument();
+
+    const editButton = screen.getByText('Edit')
+    expect(editButton).toBeInTheDocument();
+
+    const createdOnOfFirstRow = getAllByRole(firstRow, 'gridcell');
+    expect(getByText(
+      createdOnOfFirstRow[2],
+      /2022-03-02/i,
+    )).toBeInTheDocument();
+
   });
+  it('should render header buttons and column names', async () => {
+
+    render(<AccountUserList accountName="account" />, {
+      wrapper,
+    });
+
+    /**********           Number of columns :         ************/
+
+    expect(screen.getAllByRole('columnheader').length).toEqual(nbrOfColumnsExpected);
+
+    /**********           Buttons 'search' and 'Create user' exist :         ************/
+
+    const searchZone = screen.getByPlaceholderText('Search');
+    expect(searchZone).toBeInTheDocument();
+
+    const createButton = screen.getByText('Create User')
+    expect(createButton).toBeInTheDocument();
+
+    /**********           Table columns exist :         ************/
+    expect(screen.getByText('User Name')).toBeInTheDocument();
+    expect(screen.getByText('Access Keys')).toBeInTheDocument();
+    expect(screen.getByText('Created On')).toBeInTheDocument();
+
+  });
+  it('handles server error', async () => {
+    server.use(
+      rest.post('http://testendpoint/', (req, res, ctx) =>
+        res(ctx.status(500, 'error'))
+      ));
+
+    render(<AccountUserList accountName="account" />, {
+      wrapper,
+    });
+
+    await waitFor(() => screen.getByText('We failed to retrieve users, please retry later. If the error persists, please contact your support.'));
+    expect(screen.getByText('We failed to retrieve users, please retry later. If the error persists, please contact your support.')).toBeInTheDocument();
+
+  })
 });
+
