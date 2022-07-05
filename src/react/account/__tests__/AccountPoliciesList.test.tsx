61e0a006ab8826997dcdd21d1d785c01ac496cbd
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2022-07-05 15:40:56 +0200

    ZKUI-188: Add delete policy test

--- src/react/account/__tests__/AccountPoliciesList.test.tsx
+++ src/react/account/__tests__/AccountPoliciesList.test.tsx
@@ -1,5 +1,5 @@
-import { screen, waitFor } from '@testing-library/react';
-import { rest } from 'msw';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { ResponseResolver, rest } from 'msw';
 import { setupServer } from 'msw/node';
 import {
   mockOffsetSize,
@@ -33,79 +33,79 @@ const SCALITY_DATA_CONSUMER_POLICY_ID = '3I17NWO7MOCSNZ1J4V2JJFUXW18UOSJF';
 
 const nbrOfColumnsExpected = 5;
 
-const server = setupServer(
-  rest.post(`${TEST_API_BASE_URL}/`, (req, res, ctx) => {
-    const params = new URLSearchParams(req.body);
-    if (params.get('Action') === 'ListPolicyVersions') {
-      return res(
-        ctx.xml(`
-<ListPolicyVersionsResponse xmlns="https://iam.amazonaws.com/doc/2010-05-08/">
-  <ListPolicyVersionsResult>
-    <Versions>
-      <member>
-        <CreateDate>2022-06-14T12:42:46Z</CreateDate>
-        <IsDefaultVersion>true</IsDefaultVersion>
-        <VersionId>v1</VersionId>
-      </member>
-    </Versions>
-    <IsTruncated>false</IsTruncated>
-  </ListPolicyVersionsResult>
-  <ResponseMetadata>
-    <RequestId>976aa56f5944abe75a41</RequestId>
-  </ResponseMetadata>
-</ListPolicyVersionsResponse>;
-      `),
-      );
-    }
+const mockIAMApi: ResponseResolver = (req, res, ctx) => {
+  const params = new URLSearchParams(req.body);
+  if (params.get('Action') === 'ListPolicyVersions') {
     return res(
       ctx.xml(`
-<ListPoliciesResponse xmlns="https://iam.amazonaws.com/doc/2010-05-08/">
-    <ListPoliciesResult>
-        <Policies>
-            <member>
-                <PolicyName>${SCALITY_INTERNAL_POLICY_NAME}</PolicyName>
-                <DefaultVersionId>${VERSION_ID}</DefaultVersionId>
-                <PolicyId>${SCALITY_INTERNAL_POLICY_ID}</PolicyId>
-                <Path>${SCALITY_INTERNAL_USER_PATH}</Path>
-                <Arn>${SCALITY_INTERNAL_ARN}</Arn>
-                <AttachmentCount>${SCALITY_INTERNAL_ATTACHMENTS}</AttachmentCount>
-                <IsAttachable>${ATTACHABLE}</IsAttachable>
-                <CreateDate>${CREATE_DATE}</CreateDate>
-                <UpdateDate>${UPDATED_DATE}</UpdateDate>
-            </member>
-            <member>
-                <PolicyName>${NON_SCALITY_INTERNAL_POLICY_NAME}</PolicyName>
-                <DefaultVersionId>${VERSION_ID}</DefaultVersionId>
-                <PolicyId>${NON_SCALITY_INTERNAL_POLICY_ID}</PolicyId>
-                <Path>${NON_SCALITY_INTERNAL_USER_PATH}</Path>
-                <Arn>${NON_SCALITY_INTERNAL_ARN}</Arn>
-                <AttachmentCount>${NON_SCALITY_INTERNAL_ATTACHMENTS}</AttachmentCount>
-                <IsAttachable>${ATTACHABLE}</IsAttachable>
-                <CreateDate>${CREATE_DATE}</CreateDate>
-                <UpdateDate>${UPDATED_DATE}</UpdateDate>
-           </member>
-           <member>
-                <PolicyName>${SCALITY_DATA_CONSUMER_POLICY_NAME}</PolicyName>
-                <DefaultVersionId>${VERSION_ID}</DefaultVersionId>
-                <PolicyId>${SCALITY_DATA_CONSUMER_POLICY_ID}</PolicyId>
-                <Path>${SCALITY_INTERNAL_USER_PATH}</Path>
-                <Arn>${SCALITY_DATA_CONSUMER_POLICY_ARN}</Arn>
-                <AttachmentCount>${NON_SCALITY_INTERNAL_ATTACHMENTS}</AttachmentCount>
-                <IsAttachable>${ATTACHABLE}</IsAttachable>
-                <CreateDate>${CREATE_DATE}</CreateDate>
-                <UpdateDate>${UPDATED_DATE}</UpdateDate>
-           </member>
-        </Policies>
-        <IsTruncated>false</IsTruncated>
-    </ListPoliciesResult>
-    <ResponseMetadata>
-        <RequestId>61221a552b4592e5b784</RequestId>
-    </ResponseMetadata>
-</ListPoliciesResponse>
+<ListPolicyVersionsResponse xmlns="https://iam.amazonaws.com/doc/2010-05-08/">
+<ListPolicyVersionsResult>
+  <Versions>
+    <member>
+      <CreateDate>2022-06-14T12:42:46Z</CreateDate>
+      <IsDefaultVersion>true</IsDefaultVersion>
+      <VersionId>v1</VersionId>
+    </member>
+  </Versions>
+  <IsTruncated>false</IsTruncated>
+</ListPolicyVersionsResult>
+<ResponseMetadata>
+  <RequestId>976aa56f5944abe75a41</RequestId>
+</ResponseMetadata>
+</ListPolicyVersionsResponse>;
     `),
     );
-  }),
-);
+  }
+  return res(
+    ctx.xml(`
+<ListPoliciesResponse xmlns="https://iam.amazonaws.com/doc/2010-05-08/">
+  <ListPoliciesResult>
+      <Policies>
+          <member>
+              <PolicyName>${SCALITY_INTERNAL_POLICY_NAME}</PolicyName>
+              <DefaultVersionId>${VERSION_ID}</DefaultVersionId>
+              <PolicyId>${SCALITY_INTERNAL_POLICY_ID}</PolicyId>
+              <Path>${SCALITY_INTERNAL_USER_PATH}</Path>
+              <Arn>${SCALITY_INTERNAL_ARN}</Arn>
+              <AttachmentCount>${SCALITY_INTERNAL_ATTACHMENTS}</AttachmentCount>
+              <IsAttachable>${ATTACHABLE}</IsAttachable>
+              <CreateDate>${CREATE_DATE}</CreateDate>
+              <UpdateDate>${UPDATED_DATE}</UpdateDate>
+          </member>
+          <member>
+              <PolicyName>${NON_SCALITY_INTERNAL_POLICY_NAME}</PolicyName>
+              <DefaultVersionId>${VERSION_ID}</DefaultVersionId>
+              <PolicyId>${NON_SCALITY_INTERNAL_POLICY_ID}</PolicyId>
+              <Path>${NON_SCALITY_INTERNAL_USER_PATH}</Path>
+              <Arn>${NON_SCALITY_INTERNAL_ARN}</Arn>
+              <AttachmentCount>${NON_SCALITY_INTERNAL_ATTACHMENTS}</AttachmentCount>
+              <IsAttachable>${ATTACHABLE}</IsAttachable>
+              <CreateDate>${CREATE_DATE}</CreateDate>
+              <UpdateDate>${UPDATED_DATE}</UpdateDate>
+         </member>
+         <member>
+              <PolicyName>${SCALITY_DATA_CONSUMER_POLICY_NAME}</PolicyName>
+              <DefaultVersionId>${VERSION_ID}</DefaultVersionId>
+              <PolicyId>${SCALITY_DATA_CONSUMER_POLICY_ID}</PolicyId>
+              <Path>${SCALITY_INTERNAL_USER_PATH}</Path>
+              <Arn>${SCALITY_DATA_CONSUMER_POLICY_ARN}</Arn>
+              <AttachmentCount>${NON_SCALITY_INTERNAL_ATTACHMENTS}</AttachmentCount>
+              <IsAttachable>${ATTACHABLE}</IsAttachable>
+              <CreateDate>${CREATE_DATE}</CreateDate>
+              <UpdateDate>${UPDATED_DATE}</UpdateDate>
+         </member>
+      </Policies>
+      <IsTruncated>false</IsTruncated>
+  </ListPoliciesResult>
+  <ResponseMetadata>
+      <RequestId>61221a552b4592e5b784</RequestId>
+  </ResponseMetadata>
+</ListPoliciesResponse>
+  `),
+  );
+};
+
+const server = setupServer(rest.post(`${TEST_API_BASE_URL}/`, mockIAMApi));
 
 beforeAll(() => {
   server.listen({ onUnhandledRequest: 'error' });
@@ -209,17 +209,58 @@ describe('AccountPoliciesList', () => {
     });
     expect(deleteButton).toBeDisabled();
   });
-  it('should render enabled Delete button for non Scality internal policy', async () => {
+  it('should be able to delete a non Scality internal policy', async () => {
+    // S
+    const mockedDeletePolicyInterceptor = jest.fn();
+    server.use(
+      rest.post(`${TEST_API_BASE_URL}/`, (req, res, ctx) => {
+        const params = new URLSearchParams(req.body);
+        if (params.get('Action') === 'DeletePolicy') {
+          const policyARN = params.get('PolicyArn');
+          mockedDeletePolicyInterceptor(policyARN);
+          return res(ctx.xml(`<DeletePolicyResponse xmlns="https://iam.amazonaws.com/doc/2010-05-08/">
+      <ResponseMetadata>
+        <RequestId>4706281b-3d19-11e4-a4a0-cffb9EXAMPLE</RequestId>
+      </ResponseMetadata>
+    </DeletePolicyResponse>`));
+        }
+        return mockIAMApi(req, res, ctx);
+      }),
+    );
+
     reduxRender(<AccountPoliciesList accountName="account" />, {
       wrapper,
     });
 
+    // E
     await waitFor(() => screen.getAllByText(/Edit/i));
 
     const deleteButton = screen.getByRole('button', {
       name: new RegExp(`Delete ${NON_SCALITY_INTERNAL_POLICY_NAME}`, 'i'),
     });
     expect(deleteButton).not.toBeDisabled();
+
+    fireEvent.click(deleteButton);
+
+    await waitFor(() =>
+      screen.getAllByText(
+        new RegExp(
+          `Permanently remove the following policy ${NON_SCALITY_INTERNAL_POLICY_NAME} ?`,
+          'i',
+        ),
+      ),
+    );
+
+    fireEvent.click(screen.getByRole('button', { name: 'Delete' }));
+
+    await waitFor(() =>
+      expect(mockedDeletePolicyInterceptor).toHaveBeenCalled(),
+    );
+
+    // V
+    expect(mockedDeletePolicyInterceptor).toHaveBeenCalledWith(
+      NON_SCALITY_INTERNAL_ARN,
+    );
   });
   it('should render view button for non internal policies when their default version is not the latest one', async () => {
     //S
