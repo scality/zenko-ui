d7f671bd5e7c9ef9d4034512a334ce8b3d4457ef
YanJin <yanjin.cheng@scality.com>
2022-10-04 23:10:45 +0200

    ZKUI-261: Replace create data service by the new form component

--- src/react/endpoint/EndpointCreate.tsx
+++ src/react/endpoint/EndpointCreate.tsx
@@ -1,18 +1,25 @@
 import { Controller, useForm } from 'react-hook-form';
-import FormContainer, * as F from '../ui-elements/FormLayout';
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import { clearError, createEndpoint } from '../actions';
 import { useDispatch, useSelector } from 'react-redux';
 import type { AppState } from '../../types/state';
-import { Banner, Icon } from '@scality/core-ui';
-import { Button } from '@scality/core-ui/dist/next';
+import {
+  Banner,
+  Form,
+  FormGroup,
+  FormSection,
+  Icon,
+  Stack,
+} from '@scality/core-ui';
+import { Button, Input, Select } from '@scality/core-ui/dist/next';
 import Joi from '@hapi/joi';
 import { joiResolver } from '@hookform/resolvers/joi';
 import { push } from 'connected-react-router';
 import { useOutsideClick } from '../utils/hooks';
 import { renderLocation } from '../backend/location/utils';
+
 const schema = Joi.object({
-  hostname: Joi.string().label('Host Name').required().min(3),
+  hostname: Joi.string().label('Hostname').required().min(3),
   locationName: Joi.string().required(),
 });
 
@@ -21,9 +28,9 @@ function EndpointCreate() {
     register,
     handleSubmit,
     control,
-
     formState: { errors },
   } = useForm({
+    mode: 'all',
     resolver: joiResolver(schema),
   });
   const dispatch = useDispatch();
@@ -38,7 +45,7 @@ function EndpointCreate() {
     (state: AppState) => state.networkActivity.counter > 0,
   );
   const locations = useSelector(
-    (state: AppState) => state.configuration.latest.locations,
+    (state: AppState) => state.configuration.latest?.locations,
   );
 
   const clearServerError = () => {
@@ -51,7 +58,13 @@ function EndpointCreate() {
   const formRef = useRef(null);
   useOutsideClick(formRef, clearServerError);
 
-  const onSubmit = ({ hostname, locationName }) => {
+  const onSubmit = ({
+    hostname,
+    locationName,
+  }: {
+    hostname: string;
+    locationName: string;
+  }) => {
     clearServerError();
     dispatch(createEndpoint(hostname, locationName));
   };
@@ -62,87 +75,96 @@ function EndpointCreate() {
   };
 
   return (
-    <FormContainer>
-      <F.Form ref={formRef}>
-        <F.Title> Create a New Data Service </F.Title>
-        <F.Fieldset>
-          <F.Label tooltipMessages={['Cannot be modified after creation']}>
-            Hostname (endpoint)
-          </F.Label>
-          <F.Input
-            type="text"
-            id="hostname"
-            {...register('hostname')}
-            placeholder="example: s3.example.com"
-            onChange={clearServerError}
+    <Form
+      ref={formRef}
+      layout={{ kind: 'page', title: 'Create a New Data Service' }}
+      requireMode="partial"
+      rightActions={
+        <Stack gap="r16">
+          <Button
             disabled={loading}
-            autoComplete="off"
+            id="cancel-btn"
+            variant="outline"
+            onClick={handleCancel}
+            label="Cancel"
           />
-          <F.ErrorInput id="error-name" error={errors.hostname?.message} />
-        </F.Fieldset>
-        <F.Fieldset>
-          <F.Label tooltipMessages={['Cannot be modified after creation']}>
-            Select Storage Location
-          </F.Label>
-          <Controller
-            control={control}
-            id="locationName"
-            name="locationName"
-            defaultValue="us-east-1"
-            render={({ field: { onChange, value: locationName } }) => {
-              return (
-                <F.Select
-                  onChange={onChange}
-                  placeholder="Location Name"
-                  value={locationName}
-                  disabled={loading}
-                >
-                  {Object.values(locations).map((location: any, i) => (
-                    <F.Select.Option
-                      key={i}
-                      value={location.name}
-                      disabled={location?.isCold}
-                    >
-                      {renderLocation(location)}
-                    </F.Select.Option>
-                  ))}
-                </F.Select>
-              );
-            }}
+          <Button
+            disabled={loading}
+            id="create-endpoint-btn"
+            variant="primary"
+            onClick={handleSubmit(onSubmit)}
+            label="Create"
           />
-        </F.Fieldset>
-        <F.Footer>
-          <F.FooterError>
-            {hasError && (
-              <Banner
-                id="zk-error-banner"
-                icon={<Icon name="Exclamation-triangle" />}
-                title="Error"
-                variant="danger"
-              >
-                {errorMessage}
-              </Banner>
-            )}
-          </F.FooterError>
-          <F.FooterButtons>
-            <Button
+        </Stack>
+      }
+      banner={
+        hasError && (
+          <Banner
+            id="zk-error-banner"
+            icon={<Icon name="Exclamation-triangle" />}
+            title="Error"
+            variant="danger"
+          >
+            {errorMessage}
+          </Banner>
+        )
+      }
+    >
+      <FormSection>
+        <FormGroup
+          id="hostname"
+          label="Hostname (endpoint)"
+          labelHelpTooltip="Cannot be modified after creation."
+          direction="vertical"
+          error={errors.hostname?.message ?? ''}
+          content={
+            <Input
+              type="text"
+              id="hostname"
+              {...register('hostname')}
+              placeholder="s3.example.com"
+              onChange={clearServerError}
               disabled={loading}
-              id="cancel-btn"
-              variant="outline"
-              onClick={handleCancel}
-              label="Cancel"
+              autoComplete="off"
             />
-            <Button
-              disabled={loading}
-              id="create-endpoint-btn"
-              variant="primary"
-              onClick={handleSubmit(onSubmit)}
-              label="Create"
+          }
+        />
+        <FormGroup
+          id="locationName"
+          label="Storage Location"
+          direction="vertical"
+          labelHelpTooltip="Cannot be modified after creation."
+          content={
+            <Controller
+              control={control}
+              name="locationName"
+              defaultValue="us-east-1"
+              render={({ field: { onChange, value: locationName } }) => {
+                return (
+                  <Select
+                    id="locationName"
+                    onChange={onChange}
+                    placeholder="Location Name"
+                    value={locationName}
+                    disabled={loading}
+                  >
+                    {Object.values(locations || []).map((location: any, i) => (
+                      <Select.Option
+                        key={i}
+                        value={location.name}
+                        disabled={location?.isCold}
+                      >
+                        {renderLocation(location)}
+                      </Select.Option>
+                    ))}
+                  </Select>
+                );
+              }}
             />
-          </F.FooterButtons>
-        </F.Footer>
-      </F.Form>
-    </FormContainer>
+          }
+        />
+      </FormSection>
+    </Form>
   );
 }
 
