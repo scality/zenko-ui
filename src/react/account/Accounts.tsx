c81b70b2999f0065de9f13176533b705a6ca5631
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2022-10-20 10:20:45 +0200

    Fix icons

--- src/react/account/Accounts.tsx
+++ src/react/account/Accounts.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { useLocation } from 'react-router-dom';
 import AccountList from './AccountList';
 import { BreadcrumbAccount } from '../ui-elements/Breadcrumb';
 import * as L from '../ui-elements/ListLayout5';
 import Header from '../ui-elements/EntityHeader';
-import MultiAccountsLogo from '../../../public/assets/logo-multi-accounts.svg';
+import { MultiAccountsIcon } from './MultiAccountsIcon';
 import { useAccounts } from '../utils/hooks';
 
 const Accounts = () => {
@@ -16,7 +15,7 @@ const Accounts = () => {
         <BreadcrumbAccount pathname={pathname} />
       </L.BreadcrumbContainer>
       <Header
-        icon={<img src={MultiAccountsLogo} alt="Multi Accounts Logo" />}
+        icon={<MultiAccountsIcon />}
         headTitle={'All Accounts'}
         numInstance={accounts ? accounts.length : 0}
       ></Header>
