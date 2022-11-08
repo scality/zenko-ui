f54d522b0f731c4f940330a883e2cb373ceb1c28
Patrick Ear <patrick.ear@scality.com>
2022-11-08 18:31:56 +0100

    ZKUI-291 - improve config stuff

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -2,34 +2,22 @@ import '../css/index.css';
 import { history, store } from './store';
 import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import ZenkoUI from './ZenkoUI';
-// const whyDidYouRender = require('@welldone-software/why-did-you-render');
-// whyDidYouRender(React, {
-//     trackAllPureComponents: true,
-//     trackExtraHooks: [
-//         [require('react-redux/lib'), 'useSelector'],
-//     ],
-// });
-if (process.env.NODE_ENV === 'development') {
-  (async () => {
-    const { worker } = await import('../js/mock/mswBrowserMocks');
-    worker.start();
-  })();
-}
+
 export const queryClient = new QueryClient();
-const  rootElement = document.getElementById('app');
-rootElement && ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <QueryClientProvider client={queryClient}>
-        <ZenkoUI />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </ConnectedRouter>
-  </Provider>,
-  rootElement,
-);
+const rootElement = document.getElementById('app');
+rootElement &&
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <QueryClientProvider client={queryClient}>
+          <ZenkoUI />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ConnectedRouter>
+    </Provider>,
+    rootElement,
+  );
