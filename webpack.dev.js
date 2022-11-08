3c89864246a5ce9d0b31fd0eae4ef5153713a84f
Patrick Ear <patrick.ear@scality.com>
2022-11-08 17:42:01 +0100

    ZKUI-291 - prettify webpack dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,44 +1,43 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    plugins: [
-        new BundleAnalyzerPlugin(),
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, 'public/assets'),
-        host: '127.0.0.1',
-        port: 8383,
-        open: true,
-        historyApiFallback: true,
-        hot: true,
-        proxy: {
-            '/s3': {
-                target: 'http://s3.zenko.local',
-                pathRewrite: {'^/s3' : ''},
-                bypass: function(req) {
-                    req.headers.proxypath = req.path;
-                    req.headers.proxyhost = '127.0.0.1:8383';
-                },
-                changeOrigin: true,
-            },
-            '/iam': {
-                target: 'http://iam.zenko.local',
-                bypass: function(req) {
-                    req.headers.proxypath = req.path;
-                    req.headers.proxyhost = '127.0.0.1:8383';
-                },
-                changeOrigin: true,
-            },
-            '/sts': {
-                target: 'http://sts.zenko.local',
-                pathRewrite: {'^/sts' : ''},
-                changeOrigin: true,
-            },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  plugins: [new BundleAnalyzerPlugin()],
+  devServer: {
+    contentBase: path.join(__dirname, 'public/assets'),
+    host: '127.0.0.1',
+    port: 8383,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+    proxy: {
+      '/s3': {
+        target: 'http://s3.zenko.local',
+        pathRewrite: { '^/s3': '' },
+        bypass: function (req) {
+          req.headers.proxypath = req.path;
+          req.headers.proxyhost = '127.0.0.1:8383';
         },
+        changeOrigin: true,
+      },
+      '/iam': {
+        target: 'http://iam.zenko.local',
+        bypass: function (req) {
+          req.headers.proxypath = req.path;
+          req.headers.proxyhost = '127.0.0.1:8383';
+        },
+        changeOrigin: true,
+      },
+      '/sts': {
+        target: 'http://sts.zenko.local',
+        pathRewrite: { '^/sts': '' },
+        changeOrigin: true,
+      },
     },
+  },
 });
