183896c2c7466746400f4adddbae7520b67cc101
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2023-02-20 17:00:23 +0100

    test: Clear cache before each tests

--- .jest-setup-after-env.js
+++ .jest-setup-after-env.js
@@ -1,9 +1,13 @@
 import '@testing-library/jest-dom';
 import fetch from 'node-fetch';
-import { history } from './src/react/utils/test.tsx';
+import { history, queryClient } from './src/react/utils/test.tsx';
 
 window.fetch = (url, ...rest) =>
   fetch(/^https?:/.test(url) ? url : new URL(url, 'http://localhost'), ...rest);
+
+beforeEach(() => {
+  queryClient.clear();
+});
 
 afterEach(() => {
   history.push('/');
