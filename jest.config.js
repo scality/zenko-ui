e14b087786534f313b65273da0c4c5a76c263b86
YanJin <yanjin.cheng@scality.com>
2023-03-02 18:46:43 +0100

    ZKUI-312: Add pretty-bytes in transformIgnorePatterns in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   testMatch: ['**/__tests__/?(*.)+(test).ts?(x)'],
-  transformIgnorePatterns: ['/node_modules/(?!(vega-lite|@scality)/)'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(vega-lite|@scality|pretty-bytes)/)',
+  ],
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
