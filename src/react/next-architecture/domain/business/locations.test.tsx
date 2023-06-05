e13cb7a98ef4f78a248d643ae99417d154735f72
Patrick Ear <patrick.ear@scality.com>
2023-06-05 16:41:27 +0200

    ZKUI-359 - fix storage consumption

--- src/react/next-architecture/domain/business/locations.test.tsx
+++ src/react/next-architecture/domain/business/locations.test.tsx
@@ -1,6 +1,7 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { MockedAccountsLocationsAdapter } from '../../adapters/accounts-locations/MockedAccountsLocationsAdapter';
 import {
+  ACCOUNT_OWN_METRICS,
   DEFAULT_METRICS,
   DEFAULT_METRICS_MESURED_ON,
   MockedMetricsAdapter,
@@ -17,6 +18,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { PropsWithChildren } from 'react';
 import * as DSRProvider from '../../../DataServiceRoleProvider';
 import { LocationTypeKey } from '../../../../types/config';
+import { UsedCapacity } from '../../ui/metrics/LatestUsedCapacity';
 
 const defaultUsedCapacity = {
   status: 'success' as const,
@@ -252,6 +254,15 @@ describe('useListLocationsForCurrentAccount', () => {
   });
 
   it('should return only location for current account', async () => {
+    jest.spyOn(DSRProvider, 'useCurrentAccount').mockReturnValue({
+      account: {
+        id: 'account-with-own-metrics',
+        Name: 'Souris',
+        Roles: [],
+        CreationDate: DEFAULT_METRICS_MESURED_ON,
+      },
+    });
+
     // S
     const { result, waitFor } = setupAndRenderHook();
 
@@ -275,7 +286,10 @@ describe('useListLocationsForCurrentAccount', () => {
               endpoint: 'https://s3.scality.com',
               region: 'us-east-1',
             },
-            defaultUsedCapacity,
+            {
+              status: 'success',
+              value: ACCOUNT_OWN_METRICS,
+            },
           ),
         },
       },
