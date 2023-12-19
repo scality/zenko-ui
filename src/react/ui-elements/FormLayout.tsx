945067c8e0e9bf796e114e598cccc716be91f16b
Jean-Marc Millet <jean-marc.millet@scality.com>
2023-12-19 17:13:39 +0100

    remove getTheme, use new spacing
    change import

--- src/react/ui-elements/FormLayout.tsx
+++ src/react/ui-elements/FormLayout.tsx
@@ -1,13 +1,16 @@
-import { Checkbox as BasicCheckbox, TextArea, Tooltip } from '@scality/core-ui';
+import {
+  Checkbox as BasicCheckbox,
+  TextArea,
+  Tooltip,
+  spacing,
+} from '@scality/core-ui';
 import {
   LargerText,
   SmallerText,
 } from '@scality/core-ui/dist/components/text/Text.component';
 import { Select as BasicSelect } from '@scality/core-ui/dist/next';
-import { spacing } from '@scality/core-ui/dist/style/theme';
-import { getTheme } from '@scality/core-ui/dist/utils';
-import { HTMLAttributes, LabelHTMLAttributes } from 'react';
-import styled, { css } from 'styled-components';
+import { HTMLAttributes, LabelHTMLAttributes, ReactNode } from 'react';
+import styled, { CSSProperties, css } from 'styled-components';
 import { IconQuestionCircle } from './Icons';
 import { default as BasicInput } from './Input';
 import { default as BasicInputList } from './InputList';
@@ -39,7 +42,7 @@ import { default as BasicInputList } from './InputList';
 export const Title = styled(LargerText)`
   display: flex;
   text-transform: capitalize;
-  margin-bottom: ${spacing.sp16};
+  margin-bottom: ${spacing.r16};
 `;
 export const SubTitle = styled.div`
   display: flex;
@@ -60,34 +63,34 @@ export const Fieldset = styled.fieldset<{
   ${(props) => (props.alignItems ? `align-items: ${props.alignItems};` : '')}
   border: 0;
   padding: 0;
-  margin-top: ${spacing.sp12};
+  margin-top: ${spacing.r12};
 `;
 export const Select = styled(BasicSelect)`
-  margin: ${spacing.sp8} 0px ${spacing.sp4} 0px;
+  margin: ${spacing.r8} 0px ${spacing.r4} 0px;
 `;
 export const CheckboxContainer = styled.div`
   display: block;
-  margin: ${spacing.sp8} 0px ${spacing.sp4} 0px;
+  margin: ${spacing.r8} 0px ${spacing.r4} 0px;
   align-items: baseline;
 
   .sc-checkbox {
-    margin-right: ${spacing.sp8};
+    margin-right: ${spacing.r8};
   }
 `;
 export const Checkbox = styled(BasicCheckbox)``;
 export const Input = styled(BasicInput)`
-  margin: ${spacing.sp8} 0px ${spacing.sp4} 0px;
+  margin: ${spacing.r8} 0px ${spacing.r4} 0px;
 `;
 export const LargeCustomInput = styled(TextArea)`
   display: inline;
   float: left;
   margin: 1rem 0.1rem 0.3rem 0rem;
 
-  padding: ${spacing.sp1};
+  padding: ${spacing.r1};
   border-radius: 4px;
   ${(props) => {
     const { border, textSecondary, backgroundLevel1, selectedActive } =
-      getTheme(props);
+      props.theme;
     return css`
       border-color: ${border};
       color: ${textSecondary};
@@ -106,13 +109,13 @@ export const Hr = styled.hr`
   margin-bottom: 1rem;
 `;
 export const InputList = styled((props) => <BasicInputList {...props} />)`
-  margin: ${spacing.sp8} 0px ${spacing.sp4} 0px;
+  margin: ${spacing.r8} 0px ${spacing.r4} 0px;
 `;
 
 export const SessionSeperation = styled.div`
   width: 23px;
   height: 1px;
-  margin: ${spacing.sp16} 0px ${spacing.sp16} 0px;
+  margin: ${spacing.r16} 0px ${spacing.r16} 0px;
   background-color: ${(props) => props.theme.buttonSecondary};
 `;
 export const LabelSecondary = styled(SmallerText)`
@@ -120,11 +123,11 @@ export const LabelSecondary = styled(SmallerText)`
 `;
 // * ErrorInput
 const ErrorInputContainer = styled.div`
-  height: ${spacing.sp16};
+  height: ${spacing.r16};
   color: ${(props) => props.theme.statusCritical};
 `;
 const WarningInputContainer = styled.div`
-  height: ${spacing.sp16};
+  height: ${spacing.r16};
   color: ${(props) => props.theme.statusWarning};
 `;
 type ErrorInputProps = {
@@ -156,16 +159,16 @@ const LabelContainer = styled.label<{ required?: boolean }>`
 `;
 
 const RequiredField = styled.span`
-  margin-left: ${spacing.sp2};
+  margin-left: ${spacing.r2};
 `;
 
 export const TooltipContainer = styled.div`
-  margin-left: ${spacing.sp8};
+  margin-left: ${spacing.r8};
   display: inline;
 `;
 const UlOverlay = styled.ul`
   text-align: left;
-  padding: 0px 0px 0px ${spacing.sp20};
+  padding: 0px 0px 0px ${spacing.r20};
 `;
 type LabelProps = {
   children: ReactNode;
@@ -217,13 +220,13 @@ export const Footer = styled.div`
 export const FooterError = styled.div`
   flex: 1 1 auto;
   height: inherit;
-  margin-right: ${spacing.sp4};
+  margin-right: ${spacing.r4};
 `;
 export const FooterButtons = styled.div`
   flex: 0 0 auto;
 
   button {
-    margin-left: ${spacing.sp24};
+    margin-left: ${spacing.r24};
   }
 `;
 export const Form = styled.form`
@@ -236,7 +239,7 @@ export const Form = styled.form`
 `;
 export const CustomForm = styled.form`
   height: calc(100vh - 510px);
-  margin: ${spacing.sp32};
+  margin: ${spacing.r32};
 `;
 const FormContainer = styled.div`
   height: 100%;
@@ -245,7 +248,7 @@ const FormContainer = styled.div`
 export const FormScrollArea = styled.div`
   overflow: auto;
   max-height: calc(100vh - 19rem);
-  margin-right: ${spacing.sp12};
-  padding-bottom: ${spacing.sp40};
+  margin-right: ${spacing.r12};
+  padding-bottom: ${spacing.r40};
 `;
 export default FormContainer;
