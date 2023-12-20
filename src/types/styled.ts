eac0f82118504f1530e4beb2d9245ce84e363c6d
Jean-Marc Millet <jean-marc.millet@scality.com>
2023-12-20 14:24:02 +0100

    add eslint ignore to styled.ts

--- src/types/styled.ts
+++ src/types/styled.ts
@@ -2,5 +2,6 @@ import 'styled-components';
 import { CoreUITheme } from '@scality/core-ui/dist/style/theme';
 
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends CoreUITheme {}
 }
