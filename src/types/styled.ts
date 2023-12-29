8f27ce9869e40c919e34adfc0f3572c495f0697d
Jean-Marc Millet <jean-marc.millet@scality.com>
2023-12-29 17:31:20 +0100

    delete useless styled.ts after import in tsconfig

--- src/types/styled.ts
+++
@@ -1,7 +0,0 @@
-import 'styled-components';
-import { CoreUITheme } from '@scality/core-ui/dist/style/theme';
-
-declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends CoreUITheme {}
-}
