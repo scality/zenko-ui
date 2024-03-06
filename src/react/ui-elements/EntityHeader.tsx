41385c53f961635c66580d1f6503d7dcfcf3c1cf
Jean-Marc Millet <jean-marc.millet@scality.com>
2024-03-06 15:46:22 +0100

    use Icon with wrapper instead of custom icon

--- src/react/ui-elements/EntityHeader.tsx
+++ src/react/ui-elements/EntityHeader.tsx
@@ -1,6 +1,5 @@
 import { TextBadge } from './TextBadge';
-import { spacing, Stack } from '@scality/core-ui';
-import { Box } from '@scality/core-ui/dist/next';
+import { Icon, Stack } from '@scality/core-ui';
 import styled from 'styled-components';
 import { fontSize } from '@scality/core-ui/dist/style/theme';
 type Props = {
@@ -9,35 +8,16 @@ type Props = {
   numInstance: number;
 };
 
-export const HeadIcon = styled.i`
-  display: flex;
-  color: ${(props) => props.theme.infoPrimary};
-  background-color: ${(props) => props.theme.backgroundLevel1};
-  border-radius: 100%;
-  border: ${spacing.f1} solid ${(props) => props.theme.infoPrimary};
-  width: 80px;
-  height: 80px;
-  text-align: center;
-  line-height: 80px;
-  vertical-align: middle;
-  margin-right: ${spacing.f8};
-  font-size: ${fontSize.larger};
-  align-items: center;
-  justify-content: center;
-`;
-
 export const HeadTitle = styled.div`
   display: flex;
   color: ${(props) => props.theme.textSecondary};
   font-size: ${fontSize.large};
   align-items: center;
 `;
-export default function Header({ icon, headTitle, numInstance }: Props) {
+export default function Header({ headTitle, numInstance }: Props) {
   return (
     <Stack>
-      <Box display="flex">
-        <HeadIcon>{icon}</HeadIcon>
-      </Box>
+      <Icon name="Account" size="2x" withWrapper />
       <HeadTitle>
         {headTitle}
         <TextBadge text={numInstance.toString()} variant="infoPrimary" />
