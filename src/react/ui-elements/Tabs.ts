82bbc6fdc2c033dba757aefec923e04156424e38
Jean-Marc Millet <jean-marc.millet@scality.com>
2024-03-20 15:18:55 +0100

    add overflow-x property to avoid flashing table, probably due to overflow and table autosizer continuously triggering each other

--- src/react/ui-elements/Tabs.ts
+++ src/react/ui-elements/Tabs.ts
@@ -1,10 +1,11 @@
 import { Tabs } from '@scality/core-ui/dist/next';
-import { spacing } from '@scality/core-ui/dist/style/theme';
+
 import styled from 'styled-components';
 export const CustomTabs = styled(Tabs)`
   display: flex;
   flex: 1;
   .sc-tabs-item-content {
     overflow-y: auto;
+    overflow-x: scroll;
   }
 `;
