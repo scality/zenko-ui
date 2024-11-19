4e14b7393ca3cc905692d354c9e2581c89b0286f
Patrick Ear <patrick.ear@scality.com>
2024-11-19 17:08:36 +0100

    update @mf-types

--- @mf-types/shell/App.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './compiled-types/src/FederatedApp';
-export { default } from './compiled-types/src/FederatedApp';
--- @mf-types/shell/useNotificationCenter.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './compiled-types/src/useNotificationCenter';
-export { default } from './compiled-types/src/useNotificationCenter';
