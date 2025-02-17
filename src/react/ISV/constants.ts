03c38d61940ccee4bae68ea2fecd63a80bacfa69
Jean-Marc Millet <jean-marc.millet@scality.com>
2025-02-17 16:57:16 +0100

    Refactor Veeam policy constants and add GET_VEEAM_POLICY function for improved bucket policy management (use same structure as GET_COMMVAULT_POLICY)

--- src/react/ISV/constants.ts
+++ src/react/ISV/constants.ts
@@ -40,23 +40,6 @@ export const GET_VEEAM_IMMUTABLE_POLICY = (bucketName: string) =>
       },
     ],
   });
-export const VEEAM_XML_PREFIX = '.system-d26a9498-cb7c-4a87-a44a-8ae204f5ba6c';
-export const SYSTEM_XML_CONTENT = `<?xml version="1.0" encoding="UTF-8"?><SystemInfo><ProtocolVersion>"1.0"</ProtocolVersion><ModelName>"ARTESCA v1.7"</ModelName><ProtocolCapabilities><CapacityInfo>true</CapacityInfo><UploadSessions>false</UploadSessions><IAMSTS>false</IAMSTS></ProtocolCapabilities></SystemInfo>`;
-export const GET_CAPACITY_XML_CONTENT = (capacity: string) =>
-  `<?xml version="1.0" encoding="utf-8" ?><CapacityInfo><Capacity>${capacity}</Capacity><Available>0</Available><Used>0</Used></CapacityInfo>`;
-export const VEEAM_DEFAULT_ACCOUNT_NAME = 'Veeam';
-export enum VeeamApplicationType {
-  VEEAM_BACKUP_REPLICATION = 'Veeam Backup & Replication',
-  VEEAM_OFFICE_365 = 'Veeam Backup for Microsoft 365 (v6, v7)',
-  VEEAM_OFFICE_365_V8 = 'Veeam Backup for Microsoft 365 (v8+)',
-}
-export const unitChoices = {
-  GiB: 1024 ** 3,
-  TiB: 1024 ** 4,
-  PiB: 1024 ** 5,
-};
-export const VEEAM_OBJECT_KEY = `${VEEAM_XML_PREFIX}/capacity.xml`;
-export const VEEAM_SYSTEM_KEY = `${VEEAM_XML_PREFIX}/system.xml`;
 export const GET_VEEAM_NON_IMMUTABLE_POLICY = (bucketName: string) =>
   JSON.stringify({
     Version: '2012-10-17',
@@ -85,3 +68,73 @@ export const GET_VEEAM_NON_IMMUTABLE_POLICY = (bucketName: string) =>
       },
     ],
   });
+
+export const GET_VEEAM_POLICY = (buckets: string[], isImmutable: boolean) => {
+  const Sid0 = isImmutable ? 'VisualEditor0' : 'SecureBucketPolicy0';
+  const Sid1 = isImmutable ? 'VisualEditor1' : 'SecureBucketPolicy1';
+  const allowedActions = isImmutable
+    ? [
+        's3:PutObject',
+        's3:GetObject',
+        's3:DeleteObject',
+        's3:GetBucketLocation',
+        's3:GetBucketVersioning',
+        's3:GetBucketObjectLockConfiguration',
+      ]
+    : [
+        's3:GetBucketLocation',
+        's3:GetObject',
+        's3:PutObject',
+        's3:DeleteObject',
+        's3:GetBucketVersioning',
+        's3:GetBucketObjectLockConfiguration',
+        's3:ListBucketVersions',
+        's3:GetObjectVersion',
+        's3:GetObjectRetention',
+        's3:GetObjectLegalHold',
+        's3:PutObjectRetention',
+        's3:PutObjectLegalHold',
+        's3:DeleteObjectVersion',
+      ];
+  return JSON.stringify({
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Sid: Sid0,
+        Effect: 'Allow',
+        Action: allowedActions,
+        Resource: [
+          ...buckets
+            .map((bucket) => [
+              `arn:aws:s3:::${bucket}/*`,
+              `arn:aws:s3:::${bucket}`,
+            ])
+            .flat(),
+        ],
+      },
+      {
+        Sid: Sid1,
+        Effect: 'Allow',
+        Action: ['s3:ListAllMyBuckets', 's3:ListBucket'],
+        Resource: '*',
+      },
+    ],
+  });
+};
+export const VEEAM_XML_PREFIX = '.system-d26a9498-cb7c-4a87-a44a-8ae204f5ba6c';
+export const SYSTEM_XML_CONTENT = `<?xml version="1.0" encoding="UTF-8"?><SystemInfo><ProtocolVersion>"1.0"</ProtocolVersion><ModelName>"ARTESCA v1.7"</ModelName><ProtocolCapabilities><CapacityInfo>true</CapacityInfo><UploadSessions>false</UploadSessions><IAMSTS>false</IAMSTS></ProtocolCapabilities></SystemInfo>`;
+export const GET_CAPACITY_XML_CONTENT = (capacity: string) =>
+  `<?xml version="1.0" encoding="utf-8" ?><CapacityInfo><Capacity>${capacity}</Capacity><Available>0</Available><Used>0</Used></CapacityInfo>`;
+export const VEEAM_DEFAULT_ACCOUNT_NAME = 'Veeam';
+export enum VeeamApplicationType {
+  VEEAM_BACKUP_REPLICATION = 'Veeam Backup & Replication',
+  VEEAM_OFFICE_365 = 'Veeam Backup for Microsoft 365 (v6, v7)',
+  VEEAM_OFFICE_365_V8 = 'Veeam Backup for Microsoft 365 (v8+)',
+}
+export const unitChoices = {
+  GiB: 1024 ** 3,
+  TiB: 1024 ** 4,
+  PiB: 1024 ** 5,
+};
+export const VEEAM_OBJECT_KEY = `${VEEAM_XML_PREFIX}/capacity.xml`;
+export const VEEAM_SYSTEM_KEY = `${VEEAM_XML_PREFIX}/system.xml`;
