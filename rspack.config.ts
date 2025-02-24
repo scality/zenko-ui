3639a700f24083a540f485e9640ea271135f30db
Jean-Marc Millet <jean-marc.millet@scality.com>
2025-02-24 11:42:22 +0100

    Update WelcomeModal path in rspack.config.ts to reflect new component location

--- rspack.config.ts
+++ rspack.config.ts
@@ -121,7 +121,7 @@ const config: Configuration = {
       filename: 'static/js/remoteEntry.js',
       exposes: {
         './FederableApp': './src/react/FederableApp.tsx',
-        './WelcomeModal': './src/react/ui-elements/Veeam/WelcomeModal.tsx',
+        './WelcomeModal': './src/react/ISV/components/WelcomeModal.tsx',
         './SelectAccountIAMRole':
           './src/react/ui-elements/SelectAccountIAMRole.tsx',
       },
