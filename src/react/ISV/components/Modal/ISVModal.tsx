8d38524fa03cfdef49b9607a83cf62b1da81b19f
Jean-Marc Millet <jean-marc.millet@scality.com>
2025-03-14 15:21:44 +0100

    Update ISVModal continue button label logic: 'Continue' when no isv selected Adjust Veeam logo width to avoid misalignment

--- src/react/ISV/components/Modal/ISVModal.tsx
+++ src/react/ISV/components/Modal/ISVModal.tsx
@@ -169,6 +169,12 @@ const ISVModal = ({ isOpen, setIsOpen }) => {
     return <></>;
   }
 
+  const continueLabel = !selectedISV
+    ? 'Continue'
+    : selectedISV?.assistant
+    ? 'Continue to assistant'
+    : 'Continue to create account';
+
   return (
     <CustomModal
       title={
@@ -185,15 +191,12 @@ const ISVModal = ({ isOpen, setIsOpen }) => {
               variant="outline"
               label="Skip"
               onClick={() => setIsOpen(false)}
+              style={{ width: '80px' }}
             ></Button>
             <Button
               disabled={!selectedISV}
               variant="primary"
-              label={
-                selectedISV?.assistant
-                  ? 'Continue to assistant'
-                  : 'Continue to create account'
-              }
+              label={continueLabel}
               icon={<Icon name="Arrow-right"></Icon>}
               onClick={() => handleContinueClick()}
             ></Button>
--- src/react/ISV/modules/veeam/components/VeeamLogo.tsx
+++ src/react/ISV/modules/veeam/components/VeeamLogo.tsx
@@ -1,7 +1,7 @@
 export const VeeamLogo = () => (
   <svg
     height={30}
-    width={150}
+    width={130}
     viewBox="0 0 130 30"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
