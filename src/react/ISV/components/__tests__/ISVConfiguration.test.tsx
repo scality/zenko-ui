68038d2a368fa2062f69db5fc7cc2f56d4e41a31
Jean-Marc Millet <jean-marc.millet@scality.com>
2025-03-20 10:16:32 +0100

    WIP Add tests for ISV Config

--- src/react/ISV/components/__tests__/ISVConfiguration.test.tsx
+++ src/react/ISV/components/__tests__/ISVConfiguration.test.tsx
@@ -7,7 +7,7 @@ import { Veeam } from '../../modules/veeam';
 import { Commvault } from '../../modules/commvault';
 import { VeeamVBO } from '../../modules/veeam-vbo';
 import { VEEAM_OFFICE_365 } from '../../constants';
-import { Wrapper } from '../../../utils/testUtil';
+import { renderWithCustomRoute, Wrapper } from '../../../utils/testUtil';
 import { debug } from 'jest-preview';
 
 const mockNavigate = jest.fn();
@@ -26,7 +26,7 @@ jest.mock('../../hooks/useIAMUser', () => ({
   useIAMUser: jest.fn().mockReturnValue({
     isIAMUserExist: false,
     IAMUsersStatus: 'success',
-    IAMUsers: [],
+    IAMUsers: [{ id: '1', name: 'test-user' }],
     getIAMUsersMutation: { mutate: jest.fn() },
     accessKeys: null,
   }),
@@ -53,7 +53,25 @@ jest.mock('../../hooks/useCapacityUnit', () => ({
   })),
   getCapacityBytes: jest.fn(() => 1073741824), // 1GB in bytes
 }));
-
+const selectors = {
+  formTitle: () => screen.getByText('Configure ARTESCA for your Use case'),
+  createAccountRadio: () => screen.getByText(/Create a new Account/i),
+  useExistingAccountRadio: () => screen.getByText(/Use an existing Account/i),
+  skipButton: () => screen.getByText(/Skip Use case configuration/i),
+  continueButton: () => screen.getByText('Continue'),
+  useExistingAccountSelect: () =>
+    screen.getByRole('listbox', { name: /Select existing account/i }),
+  existingUserRadio: () =>
+    screen.getByRole('radio', { name: /Use an existing IAM User/i }),
+  createUserRadio: () =>
+    screen.getByRole('radio', { name: /Create a new IAM User/i }),
+  createUserInput: () =>
+    screen.getByRole('textbox', { name: /IAM User Name */i }),
+  selectExistingUser: () =>
+    screen.getByRole('listbox', { name: 'Select existing user' }),
+  generateKey: () =>
+    screen.getByRole('checkbox', { name: /Generate a new set of AK\/SK/ }),
+};
 describe('ISVConfiguration', () => {
   const mockSetConfig = jest.fn();
   const mockAccounts = {
@@ -111,7 +129,7 @@ describe('ISVConfiguration', () => {
     });
   });
 
-  describe('Account Type Selection', () => {
+  describe('Account Field', () => {
     it('should show account name input for new account', async () => {
       renderComponent();
 
@@ -136,64 +154,115 @@ describe('ISVConfiguration', () => {
         screen.queryByText('scality-internal-services'),
       ).not.toBeInTheDocument();
     });
+
+    it('should disable account selection when no accounts are available', async () => {
+      (useListAccounts as jest.Mock).mockReturnValue({ accounts: [] });
+
+      renderComponent();
+      const useExistingRadio = screen.getByLabelText(/Use an existing Account/);
+      expect(useExistingRadio).toBeDisabled();
+    });
+    it.skip('should select correct account when account is in query params and disable create a new account choice', async () => {
+      renderWithCustomRoute(
+        <ISVStepperContext.Provider
+          value={{
+            platform: Veeam,
+          }}
+        >
+          <ISVConfiguration />
+        </ISVStepperContext.Provider>,
+        '/isv/configuration?platform=veeam&account=test-account',
+      );
+      await waitFor(() => {
+        expect(screen.getByText('test-account')).toBeInTheDocument();
+      });
+      expect(screen.getByLabelText('Create a new Account')).toBeDisabled();
+    });
+    it.skip('should not disable create account if account in query params does not exist', async () => {
+      renderWithCustomRoute(
+        <ISVStepperContext.Provider
+          value={{
+            platform: Veeam,
+          }}
+        >
+          <ISVConfiguration />
+        </ISVStepperContext.Provider>,
+        '/isv/configuration?platform=veeam&account=non-existing-account',
+      );
+      expect(screen.getByLabelText('Create a new Account')).not.toBeDisabled();
+      expect(
+        screen.queryByText('non-existing-account'),
+      ).not.toBeInTheDocument();
+    });
   });
 
   describe('Platform Specific Features', () => {
-    it('should show application selection for Veeam VBO', async () => {
-      renderComponent(VeeamVBO);
-      debug();
-      waitFor(() => {
+    describe.skip('Veeam', () => {
+      it('should render correct config for Veeam', () => {
+        renderComponent(Veeam);
+
+        expect(screen.queryByText('Veeam application')).not.toBeInTheDocument();
+        // Add checks for Veeam specific fields, labels, etc.
+      });
+    });
+    describe.skip('Commvault', () => {
+      it('should render correct config for Commvault', () => {
+        renderComponent(Commvault);
+
+        expect(screen.queryByText('Veeam application')).not.toBeInTheDocument();
+        // Add checks for Commvault specific fields, labels, etc.
+      });
+    });
+    describe.skip('Veeam VBO', () => {
+      it('should render correct config for Veeam VBO', async () => {
+        renderComponent(VeeamVBO);
+
+        waitFor(() => {
+          expect(screen.getByText('Veeam application')).toBeInTheDocument();
+        });
+        // Check for Veeam application label
         expect(screen.getByText('Veeam application')).toBeInTheDocument();
+
+        // Open the dropdown
+        const applicationInput = screen.getByRole('textbox', {
+          name: 'Veeam application',
+        });
+        await userEvent.click(applicationInput);
+
+        // Check for options in the dropdown
+        const v6Options = screen.getAllByText(
+          (text) =>
+            text.includes('Veeam Backup for Microsoft 365') &&
+            text.includes('v6'),
+        );
+        expect(v6Options.length).toBeGreaterThan(0);
+
+        const v8Options = screen.getAllByText(
+          (text) =>
+            text.includes('Veeam Backup for Microsoft 365') &&
+            text.includes('v8'),
+        );
+        expect(v8Options.length).toBeGreaterThan(0);
       });
-      // Check for Veeam application label
-      expect(screen.getByText('Veeam application')).toBeInTheDocument();
+      it('should show immutable backup toggle for VBO v8+', async () => {
+        renderComponent(VeeamVBO);
 
-      // Open the dropdown
-      const applicationInput = screen.getByRole('textbox', {
-        name: 'Veeam application',
+        // Open the dropdown
+        const applicationInput = screen.getByRole('textbox', {
+          name: 'Veeam application',
+        });
+        await userEvent.click(applicationInput);
+
+        // Select v8+ application using flexible text matching
+        const v8Option = screen.getByText(
+          (text) =>
+            text.includes('Veeam Backup for Microsoft 365') &&
+            text.includes('v8'),
+        );
+        await userEvent.click(v8Option);
+
+        expect(screen.getByText('Immutable Backup')).toBeInTheDocument();
       });
-      await userEvent.click(applicationInput);
-
-      // Check for options in the dropdown
-      const v6Options = screen.getAllByText(
-        (text) =>
-          text.includes('Veeam Backup for Microsoft 365') &&
-          text.includes('v6'),
-      );
-      expect(v6Options.length).toBeGreaterThan(0);
-
-      const v8Options = screen.getAllByText(
-        (text) =>
-          text.includes('Veeam Backup for Microsoft 365') &&
-          text.includes('v8'),
-      );
-      expect(v8Options.length).toBeGreaterThan(0);
-    });
-
-    it('should not show application selection for Commvault', () => {
-      renderComponent(Commvault);
-
-      expect(screen.queryByText('Veeam application')).not.toBeInTheDocument();
-    });
-
-    it('should show immutable backup toggle for VBO v8+', async () => {
-      renderComponent(VeeamVBO);
-
-      // Open the dropdown
-      const applicationInput = screen.getByRole('textbox', {
-        name: 'Veeam application',
-      });
-      await userEvent.click(applicationInput);
-
-      // Select v8+ application using flexible text matching
-      const v8Option = screen.getByText(
-        (text) =>
-          text.includes('Veeam Backup for Microsoft 365') &&
-          text.includes('v8'),
-      );
-      await userEvent.click(v8Option);
-
-      expect(screen.getByText('Immutable Backup')).toBeInTheDocument();
     });
   });
 
@@ -241,7 +310,6 @@ describe('ISVConfiguration', () => {
         expect(continueButton).toBeDisabled();
       });
     });
-
     it('should show error for duplicate account names', async () => {
       renderComponent();
 
@@ -254,9 +322,49 @@ describe('ISVConfiguration', () => {
         screen.getByText('Account name already exists'),
       ).toBeInTheDocument();
     });
+    // TODO
+    it('should show error for duplicate IAM User Names', async () => {});
+    it('should show error for duplicate bucket names', async () => {
+      renderComponent();
+
+      // Fill in bucket name
+      await userEvent.clear(
+        screen.getByRole('textbox', { name: 'Bucket name *' }),
+      );
+      await userEvent.type(
+        screen.getByRole('textbox', { name: 'Bucket name *' }),
+        'test-bucket-1',
+      );
+
+      // Add buckets
+      const bucketNumberInput = screen.getByRole('spinbutton', {
+        name: 'Number of buckets *',
+      });
+
+      await userEvent.type(bucketNumberInput, '2');
+
+      expect(bucketNumberInput).toHaveValue();
+
+      waitFor(() => {
+        expect(screen.getByText('Bucket #2 name *')).toBeInTheDocument();
+      });
+
+      // Fill in second bucket name
+      await userEvent.clear(
+        screen.getByRole('textbox', { name: 'Bucket #2 name *' }),
+      );
+      await userEvent.type(
+        screen.getByRole('textbox', { name: 'Bucket #2 name *' }),
+        'test-bucket-1',
+      );
+
+      expect(
+        screen.getByText('Bucket name must be unique'),
+      ).toBeInTheDocument();
+    });
   });
 
-  describe('Advanced Settings', () => {
+  describe.only('Advanced Settings', () => {
     it('should show IAM user management section when using existing account', async () => {
       renderComponent();
 
@@ -294,10 +402,21 @@ describe('ISVConfiguration', () => {
       await userEvent.click(screen.getByText('Advanced settings'));
 
       // Test switching between IAM user types
+      waitFor(() => {
+        expect(screen.getByText('Create a new IAM User')).toBeInTheDocument();
+      });
+
       await userEvent.click(screen.getByText('Create a new IAM User'));
+      expect(screen.getByLabelText('Create a new IAM User')).toBeChecked();
       expect(
-        screen.getByLabelText('Create a new IAM User'),
-      ).toBeInTheDocument();
+        screen.queryByRole('textbox', { name: /IAM User/i }),
+      ).toBeVisible();
+
+      await userEvent.click(screen.getByText('Use an existing IAM User'));
+      expect(screen.getByLabelText('Use an existing IAM User')).toBeChecked();
+      expect(
+        screen.queryByRole('listbox', { name: /Select existing user/ }),
+      ).toBeVisible();
     });
 
     it('should show generate key checkbox for existing IAM user', async () => {
@@ -317,6 +436,109 @@ describe('ISVConfiguration', () => {
       expect(
         screen.getByText('Generate a new set of AK/SK'),
       ).toBeInTheDocument();
+    });
+    it('should not check by default generate key checkbox for existing IAM user with active keys', async () => {
+      renderComponent();
+
+      // Select existing account
+      await userEvent.click(screen.getByText('Use an existing Account'));
+      await userEvent.click(screen.getByText('Select existing account'));
+      await userEvent.click(screen.getByText('test-account'));
+
+      // Open advanced settings
+      await userEvent.click(screen.getByText('Advanced settings'));
+
+      // Select existing IAM user
+      await userEvent.click(screen.getByText('Use an existing IAM User'));
+      await userEvent.click(
+        screen.queryByRole('listbox', { name: /Select existing user/ }),
+      );
+      await userEvent.click(screen.getByText('test-user'));
+
+      expect(
+        screen.getByText('Generate a new set of AK/SK'),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByLabelText('Generate a new set of AK/SK'),
+      ).not.toBeChecked();
+    });
+
+    it.only('should disable and check generate key checkbox for existing IAM user with no active keys', async () => {
+      renderComponent();
+
+      // Select existing account
+      await userEvent.click(screen.getByText('Use an existing Account'));
+      await userEvent.click(screen.getByText('Select existing account'));
+      await userEvent.click(screen.getByText('test-account'));
+
+      // Open advanced settings
+      await userEvent.click(screen.getByText('Advanced settings'));
+
+      // Select existing IAM user
+      await userEvent.click(screen.getByText('Use an existing IAM User'));
+      await userEvent.click(
+        screen.queryByRole('listbox', { name: /Select existing user/ }),
+      );
+      await userEvent.click(screen.getByText('test-user'));
+
+      expect(
+        screen.getByText('Generate a new set of AK/SK'),
+      ).toBeInTheDocument();
+      debug();
+
+      await waitFor(
+        () => {
+          expect(
+            screen.getByLabelText(/Generate a new set of AK\/SK/),
+          ).toBeChecked();
+        },
+        { timeout: 10000 },
+      );
+
+      expect(
+        screen.getByLabelText('Generate a new set of AK/SK'),
+      ).toBeDisabled();
+    });
+
+    //TODO
+    it('should select create new IAM User if IAM User does not exist and prefill it with account name', async () => {
+      renderComponent();
+      await userEvent.click(screen.getByText('Use an existing Account'));
+      await userEvent.click(screen.getByText('Select existing account'));
+      await userEvent.click(screen.getByText('test-account'));
+      await userEvent.click(screen.getByText('Advanced settings'));
+      waitFor(() => {
+        expect(selectors.createUserRadio()).toBeChecked();
+        expect(selectors.existingUserRadio()).not.toBeChecked();
+        expect(selectors.createUserInput()).toHaveValue('test-account');
+      });
+    });
+
+    it('should select IAM User corresponding to account name if it exist', async () => {
+      renderComponent();
+      await userEvent.click(screen.getByText('Use an existing Account'));
+      await userEvent.click(screen.getByText('Select existing account'));
+      await userEvent.click(screen.getByText('test-account'));
+
+      await userEvent.click(screen.getByText('Advanced settings'));
+      waitFor(() => {
+        expect(screen.getByText(/IAM User Management /)).toBeVisible();
+      });
+      debug();
+      expect(selectors.createUserRadio()).not.toBeChecked();
+      expect(selectors.existingUserRadio()).toBeChecked();
+      expect(selectors.selectExistingUser()).toHaveValue('test-account');
+    });
+    it('should open and focus select user if selecting account with no user corresponding to account name', async () => {
+      renderComponent();
+      await userEvent.click(screen.getByText('Use an existing Account'));
+      await userEvent.click(screen.getByText('Select existing account'));
+      await userEvent.click(screen.getByText('test-account'));
+
+      waitFor(() => {
+        expect(screen.getByText('Select existing user')).toBeInTheDocument();
+        expect(screen.getByText('Select existing user')).toHaveFocus();
+      });
     });
   });
 
@@ -386,21 +608,21 @@ describe('ISVConfiguration', () => {
       expect(screen.getByText('Exit Veeam assistant?')).toBeInTheDocument();
     });
 
-    // it('should navigate to accounts page when confirming skip', async () => {
-    //   renderComponent();
+    it('should navigate to accounts page when confirming skip', async () => {
+      renderComponent();
 
-    //   // Click the skip button
-    //   await userEvent.click(screen.getByText('Skip Use case configuration'));
+      // Click the skip button
+      await userEvent.click(screen.getByText('Skip Use case configuration'));
 
-    //   // Click the "Exit" button in the modal
-    //   await userEvent.click(
-    //     screen.getByRole('button', { name: 'Exit configuration' }),
-    //   );
+      // Click the "Exit" button in the modal
+      await userEvent.click(
+        screen.getByRole('button', { name: 'Exit configuration' }),
+      );
 
-    //   await waitFor(() => {
-    //     expect(mockNavigate).toHaveBeenCalledWith('/accounts');
-    //   });
-    // });
+      await waitFor(() => {
+        expect(mockNavigate).toHaveBeenCalledWith('/accounts');
+      });
+    });
 
     it('should close modal without navigating when canceling skip', async () => {
       renderComponent();
