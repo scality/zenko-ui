b62dc2da308956bcfa9173982ae4d9cf47aef718
Jean-Marc Millet <jean-marc.millet@scality.com>
2025-03-20 11:54:32 +0100

    Remove duplicate text in tooltip for IAMUser

--- src/react/ISV/components/IAMUserTooltip.tsx
+++ src/react/ISV/components/IAMUserTooltip.tsx
@@ -18,11 +18,6 @@ export const IAMUSerTooltip = ({ platform }: { platform: string }) => {
           existing AK/SK).`}
         </ListItem>
         <ListItem>
-          {`If a IAM User created via the ${platform} assistant already exists, it
-          is recommended to use it (${platform} should recognize and use its
-          existing AK/SK).`}
-        </ListItem>
-        <ListItem>
           {`If no IAM User is available in the account (or if it's a new account),
           a new user will be created automatically, and its AK/SK will be
           provided at the summary step. If needed (like key loss or rotation), a
