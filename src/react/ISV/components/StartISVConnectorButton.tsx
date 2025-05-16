91e6aeafd9e8806d1e67e25dbe0098d7a0bf8616
YanJin <yanjin.cheng@scality.com>
2025-05-16 18:52:16 +0200

    Artesca-15205: Go to the VBR assistant from the bucket \ account page

--- src/react/ISV/components/StartISVConnectorButton.tsx
+++ src/react/ISV/components/StartISVConnectorButton.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import ISVModal from './Modal/ISVModal';
 import { Button } from '@scality/core-ui/dist/next';
+import { useBasenameRelativeNavigate } from '@scality/module-federation';
 import { useIsVeeamVBROnly } from '../hooks/useIsVeeamVBROnly';
 
 export const StartISVConnectorButton = () => {
+  const navigate = useBasenameRelativeNavigate();
   const [isISVModalOpen, setIsISVModalOpen] = useState(false);
   const isVeeamVBROnly = useIsVeeamVBROnly();
 
@@ -15,7 +17,13 @@ export const StartISVConnectorButton = () => {
           isVeeamVBROnly ? 'Start Veeam VBR Assistant' : 'Start ISV Connector'
         }
         variant="secondary"
-        onClick={() => setIsISVModalOpen(true)}
+        onClick={() => {
+          if (isVeeamVBROnly) {
+            navigate('/isv/configuration?platform=veeam-vbr');
+          } else {
+            setIsISVModalOpen(true);
+          }
+        }}
         type="button"
       />
     </>
