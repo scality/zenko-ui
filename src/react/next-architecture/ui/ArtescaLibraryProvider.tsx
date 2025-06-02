49749c83dc7abc4a3ffbaa85c8a09a9e694943ab
Jean-Marc Millet <jean-marc.millet@scality.com>
2025-06-02 19:41:31 +0200

    Import federated hook library from Artesca

---
+++ src/react/next-architecture/ui/ArtescaLibraryProvider.tsx
@@ -0,0 +1,76 @@
+import { ErrorPage500 } from '@scality/core-ui/dist/components/error-pages/ErrorPage500.component';
+import * as ModuleFederation from '@scality/module-federation';
+import { ErrorBoundary } from 'react-error-boundary';
+
+export const ARTESCA_LIBRARY_NOT_AVAILABLE = 'Artesca library is not available';
+export class ArtescaLibraryNotAvailable extends Error {
+  constructor() {
+    super(ARTESCA_LIBRARY_NOT_AVAILABLE);
+  }
+}
+const artescaLibraryGlobal = {};
+export function useArtescaLibrary() {
+  //@ts-expect-error fix this when you are working on it
+  if (artescaLibraryGlobal.hooks) {
+    //@ts-expect-error fix this when you are working on it
+    return artescaLibraryGlobal.hooks;
+  }
+
+  return new ArtescaLibraryNotAvailable();
+}
+
+const InternalArtescaLibraryProvider = ({
+  moduleExports,
+  children,
+}: {
+  moduleExports: Record<string, never>;
+  children: React.ReactNode;
+}) => {
+  //@ts-expect-error fix this when you are working on it
+  artescaLibraryGlobal.hooks = moduleExports['./hooks/artescaLibrary'];
+  return <>{children}</>;
+};
+
+function ErrorFallback() {
+  return <ErrorPage500 data-cy="sc-error-page500" locale={'en'} />;
+}
+
+export function ArtescaLibraryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { useDeployedApps } = ModuleFederation.useShellHooks();
+  const instances = useDeployedApps({ kind: 'artesca-base-ui' });
+
+  const { useConfig } = ModuleFederation.useShellHooks();
+  const artescaWebfinger = useConfig({
+    configType: 'build',
+    name: instances[0].name,
+  });
+
+  const federatedImports = instances.map((instance) => {
+    return {
+      scope: artescaWebfinger.spec.hooks.artescaLibrary.scope,
+      module: artescaWebfinger.spec.hooks.artescaLibrary.module,
+      remoteEntryUrl: `${instance.url}${artescaWebfinger.spec.remoteEntryPath}?version=${instance.version}`,
+    };
+  });
+
+  if (!instances.length) {
+    return <>{children}</>;
+  } else {
+    return (
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <ModuleFederation.ComponentWithFederatedImports
+          componentWithInjectedImports={InternalArtescaLibraryProvider}
+          renderOnError={<ErrorPage500 />}
+          componentProps={{
+            children,
+          }}
+          federatedImports={federatedImports}
+        />
+      </ErrorBoundary>
+    );
+  }
+}
