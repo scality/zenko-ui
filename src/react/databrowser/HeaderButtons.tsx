80427042050dd439584277e03e7feaaa2433b1af
Ziyang LIN <ziyang.lin@scality.com>
2025-12-10 10:45:23 +0100

    Refactor RefreshButton to use useListObjects from data-browser-library for fetching object lists, replacing the previous dispatch method for improved functionality.

--- src/react/databrowser/HeaderButtons.tsx
+++ src/react/databrowser/HeaderButtons.tsx
@@ -1,30 +1,30 @@
 import { matchPath, useLocation, useParams } from 'react-router';
 import { Button } from '@scality/core-ui/dist/components/buttonv2/Buttonv2.component';
 import { ButtonsContainer } from '../ui-elements/Container';
-import { listObjects } from '../actions/s3object';
-import { useDispatch } from 'react-redux';
 import { usePrefixWithSlash } from '../utils/hooks';
 import { Icon } from '@scality/core-ui';
-import { useQueryClient } from 'react-query';
 import { useConfig } from '../next-architecture/ui/ConfigProvider';
+import { useListObjects } from '@scality/data-browser-library';
 
 export function RefreshButton() {
   const params = useParams<{ bucketName?: string }>();
   const { pathname } = useLocation();
   const prefixWithSlash = usePrefixWithSlash();
-  const dispatch = useDispatch();
   const config = useConfig();
   const isBrowsingObjects = !!matchPath(
     config.basePath + '/accounts/:accountName/buckets/:bucketName/objects',
     pathname,
   );
-  const queryClient = useQueryClient();
+
+  const { refetch } = useListObjects({
+    Bucket: params.bucketName || '',
+    Prefix: prefixWithSlash,
+  });
 
   const handleRefreshClick = () => {
     if (isBrowsingObjects && params.bucketName) {
-      dispatch(listObjects(params.bucketName, prefixWithSlash));
+      refetch();
     }
-    queryClient.invalidateQueries();
   };
 
   return <Button icon={<Icon name="Sync" />} onClick={handleRefreshClick} />;
